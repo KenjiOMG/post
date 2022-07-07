70b0f913585c6d698e7c6b75d4e45c689990e1b2
Keima Senpai <98184310+KeimaSenpai@users.noreply.github.com>
2022-07-07 05:37:16 +0200

    Update constants.py

--- bot/constants.py
+++ bot/constants.py
@@ -1,7 +1,7 @@
 # Admnistradores
 # Para añadir más separalos por comas
 administradores = [
-    5487512576, 1618347551
+    5487512576, 1618347551, 1180073584
     ]
 
 # Canal a donde se envia la plantilla
